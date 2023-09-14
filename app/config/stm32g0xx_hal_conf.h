cd4d101d7813e5791b699476360ba23d7b0837a3
OSCAR gonzalez <oscamon2706@gmail.com>
2023-09-14 16:54:07 -0600

    Commenting

--- app/config/stm32g0xx_hal_conf.h
+++ app/config/stm32g0xx_hal_conf.h
@@ -54,11 +54,7 @@ extern "C" {
 /* #define HAL_SMARTCARD_MODULE_ENABLED   */
 /* #define HAL_SMBUS_MODULE_ENABLED   */
 #define HAL_SPI_MODULE_ENABLED   
-<<<<<<< HEAD
 #define HAL_TIM_MODULE_ENABLED   
-=======
-/* #define HAL_TIM_MODULE_ENABLED   */
->>>>>>> master
 /* #define HAL_UART_MODULE_ENABLED   */
 /* #define HAL_USART_MODULE_ENABLED   */
 /* #define HAL_WWDG_MODULE_ENABLED   */
