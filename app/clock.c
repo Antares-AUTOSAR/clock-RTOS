e44867e5678788b9ede7b5677a04e84ead3330fa
Angel Lozano <angel_ivan_5@outlook.com>
2023-09-16 02:13:09 -0600

    Replaced RTC initialization values with defines and created timer

--- app/clock.c
+++ app/clock.c
@@ -19,6 +19,7 @@ static void state_serialMsgDate( APP_MsgTypeDef *receivedMessage );
 static void state_serialMsgTime( APP_MsgTypeDef *receivedMessage );
 static void state_clockMsgPrint( APP_MsgTypeDef *receivedMessage );
 
+static void Clock_Update_DateAndTime( TimerHandle_t pxTimer );
 
 /**
  * @brief  Function to trigger the event machine
@@ -39,6 +40,12 @@ static uint8_t dateYearH = 19;
 
 
 /**
+ * @brief  Struct for handling RTC peripheral Declared in bsp
+ */
+TimerHandle_t xTimerDisplay = { 0 };
+
+
+/**
  * @brief   Use once to Initialize the clock
  *
  * Initializes RTC peripheral using 32.768kHz external clock
@@ -52,27 +59,30 @@ void Clock_Init( void )
 
     RtcHandler.Instance            = RTC;
     RtcHandler.Init.HourFormat     = RTC_HOURFORMAT_24;
-    RtcHandler.Init.AsynchPrediv   = 127;
-    RtcHandler.Init.SynchPrediv    = 255;
+    RtcHandler.Init.AsynchPrediv   = RTC_INIT_PASYNC;
+    RtcHandler.Init.SynchPrediv    = RTC_INIT_PSYNC;
     RtcHandler.Init.OutPut         = RTC_OUTPUT_DISABLE;
     RtcHandler.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
     RtcHandler.Init.OutPutType     = RTC_OUTPUT_TYPE_OPENDRAIN;
     HAL_RTC_Init( &RtcHandler );
 
-    sTime.Hours          = 0x12;
-    sTime.Minutes        = 0x00;
-    sTime.Seconds        = 0x00;
-    sTime.SubSeconds     = 0x00;
+    sTime.Hours          = RTC_INITIAL_HOUR;
+    sTime.Minutes        = RTC_INITIAL_MIN;
+    sTime.Seconds        = RTC_INITIAL_SEC;
+    sTime.SubSeconds     = RTC_INITIAL_SSEC; // Initial time 12:00:00.0
     sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
     sTime.StoreOperation = RTC_STOREOPERATION_RESET;
     HAL_RTC_SetTime( &RtcHandler, &sTime, RTC_FORMAT_BCD );
 
     sDate.Month   = RTC_MONTH_JANUARY;
     sDate.WeekDay = RTC_WEEKDAY_SATURDAY;
-    sDate.Date    = 0x01;
-    sDate.Year    = 0x99;
-    dateYearH     = 19;
+    sDate.Date    = RTC_INITIAL_MDAY;
+    sDate.Year    = RTC_INITIAL_YEAR;
+    dateYearH     = RTC_INITIAL_YEARH; // Initial date JAN1 2000
     HAL_RTC_SetDate( &RtcHandler, &sDate, RTC_FORMAT_BCD );
+
+    xTimerDisplay = xTimerCreate( "D_Timer", 1000, pdTRUE, TIMER_DISPLAY_ID, Clock_Update_DateAndTime );
+    xTimerStart( xTimerDisplay, 0 );
 }
 
 
@@ -141,10 +151,11 @@ static void state_serialMsgDate( APP_MsgTypeDef *receivedMessage )
 
     RTC_DateTypeDef sDate = { 0 };
 
-    sDate.Date  = receivedMessage->tm.tm_mday;
-    sDate.Month = receivedMessage->tm.tm_mon;
-    sDate.Year  = receivedMessage->tm.tm_year % 100UL;
-    dateYearH   = receivedMessage->tm.tm_year / 100UL;
+    sDate.Date    = receivedMessage->tm.tm_mday;
+    sDate.Month   = receivedMessage->tm.tm_mon;
+    sDate.Year    = receivedMessage->tm.tm_year % 100UL;
+    sDate.WeekDay = receivedMessage->tm.tm_wday;
+    dateYearH     = receivedMessage->tm.tm_year / 100UL;
     HAL_RTC_SetDate( &RtcHandler, &sDate, RTC_FORMAT_BIN );
 
     clockMessage.msg = CLOCK_MSG_PRINT;
@@ -202,10 +213,13 @@ static void state_clockMsgPrint( APP_MsgTypeDef *receivedMessage )
 
 /**
  * @brief   Function executed every second to print data in LCD
+ * @param   pxTimer Timer handler
  */
-void Clock_Update_DateAndTime( void )
+void Clock_Update_DateAndTime( TimerHandle_t pxTimer )
 {
     static APP_MsgTypeDef clockMessage = { 0 };
+
+    UNUSED( pxTimer );
 
     clockMessage.msg = CLOCK_MSG_PRINT;
     xQueueSend( clockQueue, &clockMessage, 0 );
