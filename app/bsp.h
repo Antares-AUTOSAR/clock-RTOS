58c562a308386949bffc1e136292b49bb09b599e
Angel Lozano <angel_ivan_5@outlook.com>
2023-09-16 22:40:28 -0600

    Improved struct members naming

--- app/bsp.h
+++ app/bsp.h
@@ -13,17 +13,17 @@
  */
 typedef struct _APP_TmTypeDef
 {
-    uint32_t tm_sec;    /**< seconds,  range 0 to 59          */
-    uint32_t tm_min;    /**< minutes, range 0 to 59           */
-    uint32_t tm_hour;   /**< hours, range 0 to 23             */
-    uint32_t tm_mday;   /**< day of the month, range 1 to 31  */
-    uint32_t tm_mon;    /**< month, range 0 to 11             */
-    uint32_t tm_year;   /**< years in rage 1900 2100          */
-    uint32_t tm_wday;   /**< day of the week, range 0 to 6    */
-    uint32_t tm_yday;   /**< day in the year, range 0 to 365  */
-    uint32_t tm_isdst;  /**< daylight saving time             */
-    uint32_t tm_hour_a; /**< alarm hour,  range 0 to 23       */
-    uint32_t tm_min_a;  /**< alarm minutes,  range 0 to 59    */
+    uint32_t tm_sec;        /**< seconds,  range 0 to 59          */
+    uint32_t tm_min;        /**< minutes, range 0 to 59           */
+    uint32_t tm_hour;       /**< hours, range 0 to 23             */
+    uint32_t tm_mday;       /**< day of the month, range 1 to 31  */
+    uint32_t tm_mon;        /**< month, range 0 to 11             */
+    uint32_t tm_year;       /**< years in rage 1900 2100          */
+    uint32_t tm_wday;       /**< day of the week, range 0 to 6    */
+    uint32_t tm_yday;       /**< day in the year, range 0 to 365  */
+    uint32_t tm_isdst;      /**< daylight saving time             */
+    uint32_t tm_hour_alarm; /**< alarm hour,  range 0 to 23       */
+    uint32_t tm_min_alarm;  /**< alarm minutes,  range 0 to 59    */
 } APP_TmTypeDef;
 
 
