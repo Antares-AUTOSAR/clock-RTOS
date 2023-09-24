9c9a7004abcee374e6b8b4beb83cd8a7c68f4c7a
Angel Lozano <angel_ivan_5@outlook.com>
2023-09-23 18:29:28 -0600

    Template for testing clock

--- test/test_app_clock.c
+++ test/test_app_clock.c
@@ -10,12 +10,19 @@
 
 void setUp( void )
 {
+    mock_stm32g0xx_hal_rtc_Init( );
+    mock_stm32g0xx_hal_rtc_ex_Init( );
 }
 
 void tearDown( void )
 {
 }
 
-void test__( void )
+void test__Clock_Init( void )
 {
+    int res;
+
+    res = 16;
+
+    TEST_ASSERT_EQUAL_MESSAGE( 16, res, "Ret value must be 16" );
 }
