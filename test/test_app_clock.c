f0052dcac3e151b65f433600e71f95e70f6ced25
Angel Lozano <angel_ivan_5@outlook.com>
2023-09-23 18:01:04 -0600

    Working template for unit testing. All mocks generated successfully

---
+++ test/test_app_clock.c
@@ -0,0 +1,21 @@
+#include "unity.h"
+#include "app_clock.h"
+
+#include "FreeRTOS.h"
+#include "mock_stm32g0xx_hal_rtc.h"
+#include "mock_stm32g0xx_hal_rtc_ex.h"
+#include "mock_timers.h"
+#include "mock_task.h"
+#include "mock_queue.h"
+
+void setUp( void )
+{
+}
+
+void tearDown( void )
+{
+}
+
+void test__( void )
+{
+}
