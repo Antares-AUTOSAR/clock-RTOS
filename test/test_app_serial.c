a196316ab7a3d9a0203632df8b70210d42cf039f
ChristopherBogarin <christopher.bogarint@gmail.com>
2023-10-17 17:19:18 -0600

    Null

--- test/test_app_serial.c
+++ test/test_app_serial.c
@@ -1,10 +1,14 @@
 #include "unity.h"
 #include "app_serial.h"
-#include "bsp.h"
+
 
 #include "FreeRTOS.h"
+#include "mock_stm32g0xx_hal_fdcan.h"
 #include "mock_queue.h"
 
+//#include "bsp.h"
+
+//void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs);
 
 void setUp( void )
 {
@@ -14,10 +18,33 @@ void tearDown( void )
 {
 }
 
+/*Define queue for serial*/
+QueueHandle_t serialQueue;
+
 /* Define queue for clock */
 QueueHandle_t clockQueue;
 
-void serialInit__testInit( void )
+void test__Serial_Init__testInit( void )
 {
-    Serial_Init( );
+    HAL_FDCAN_Init_IgnoreAndReturn( HAL_OK );
+    HAL_FDCAN_ConfigFilter_IgnoreAndReturn( HAL_OK );
+    HAL_FDCAN_ConfigGlobalFilter_IgnoreAndReturn( HAL_OK );
+    HAL_FDCAN_Start_IgnoreAndReturn( HAL_OK );
+    HAL_FDCAN_ActivateNotification_IgnoreAndReturn( HAL_OK );
+
+    Serial_Init();
 }
+
+/*void test__HAL_FDCAN__RxFifo0Callback(void)
+{
+
+    //FDCAN_HandleTypeDef CANHandler;
+    //FDCAN_HandleTypeDef *hfdcan;
+    //hfdcan = &CANHandler;
+   // uint32_t RxFifo0ITs = 1;
+
+    //HAL_FDCAN_GetRxMessage_IgnoreAndReturn( HAL_OK);
+    //xQueueGenericSend_IgnoreAndReturn( pdPASS );
+
+    //HAL_FDCAN_RxFifo0Callback( hfdcan, RxFifo0ITs);
+}*/
